eb740f9851a73d12a96cecd8610e030ae26e7cd9
Arpitha4 <arpithabhatng@gmail.com>
2025-10-15 20:59:40 +0530

    update

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -45,7 +45,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+# Optional: enable gzip compression
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ROOT URL
 ROOT_URLCONF = 'food_delivery.urls'
